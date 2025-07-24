374ed2372e5d87b6894275a86f11e80a2cff22cd
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-07-24 18:02:06 +0200

    refactor: ItemStack codec to a concrete class

--- src/main/java/ovh/paulem/krimson/codec/Codecs.java
+++ src/main/java/ovh/paulem/krimson/codec/Codecs.java
@@ -1,40 +1,9 @@
 package ovh.paulem.krimson.codec;
 
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import ovh.paulem.krimson.common.compat.serialize.ItemSerializerHandler;
-import ovh.paulem.krimson.common.compat.stream.input.InputStreamHandler;
-import ovh.paulem.krimson.common.compat.stream.output.OutputStreamHandler;
-import ovh.paulem.krimson.compat.CompatAccess;
+import ovh.paulem.krimson.codec.impl.ItemStackCodec;
 
 public interface Codecs {
-    ZLibCodec<ItemStack> ITEM_STACK_CODEC = new ZLibCodec<>() {
-        @Override
-        protected OutputStreamHandler<?> createEncoder(@NotNull OutputStreamHandler<?> dataOutput, ItemStack object) throws Exception {
-            if (object == null) {
-                // Ensure the correct order by including empty/null items
-                // Simply remove the write line if you don't want this
-                dataOutput.writeInt(0);
-                return dataOutput;
-            }
-
-            ItemSerializerHandler itemSerializer = CompatAccess.getHandler();
-            itemSerializer.serializeAndWrite(object, dataOutput);
-            return dataOutput;
-        }
-
-        @Override
-        public ItemStack decode(@NotNull InputStreamHandler<?> dataInput, byte @Nullable [] object) throws Exception {
-            int length = dataInput.readInt();
-
-            if(length == 0) return null;
-
-            ItemSerializerHandler itemSerializer = CompatAccess.getHandler();
-            return itemSerializer.readAndDeserialize(dataInput, length);
-        }
-    };
-
+    ZLibCodec<ItemStack> ITEM_STACK_CODEC = new ItemStackCodec();
     Codec<ItemStack, byte[]> ITEM_STACK_BASE_CODEC = ITEM_STACK_CODEC.toBasicCodec();
-
 }
